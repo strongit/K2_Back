abdab7c4f26286104154eb46f8d2d9846bdda2e9
xiaof <xiaof@wangsu.com>
2018-04-13 17:57:09 +0800

    update

--- apps/users/views.py
+++ apps/users/views.py
@@ -58,7 +58,6 @@ class UserViewSet(mixins.CreateModelMixin,  mixins.ListModelMixin, mixins.Update
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
-        print(request.data)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
 
